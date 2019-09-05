e740fdeeb475f7b5cf6d87b6ec7baf6eb38ce503
dracstorey <40627800+dracstorey@users.noreply.github.com>
2019-09-05 11:47:30 +0100

    Create sum_of_two.py

---
+++ sum_of_two.py
@@ -0,0 +1,11 @@
+#Programme to add two numbers together
+
+# Input two numbers
+
+iNum1 = int(input("Input an integer"))
+iNum2 = int(input("Input an integer"))
+
+#output sum of numbers
+
+print (iNum1 + iNum2)
+
